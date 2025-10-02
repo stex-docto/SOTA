1f4fe9bf53f13e4dd16b31460e4500905da7008e
Thomas de Saint Exupery <thomas.desaintexupery@doctolib.com>
2025-10-02 11:29:21 +0200

    feat: better sign-out modal

--- src/presentation/components/AuthModal.tsx
+++ src/presentation/components/AuthModal.tsx
@@ -3,8 +3,8 @@ import { useDependencies } from '../hooks/useDependencies'
 import { useAuth } from '../hooks/useAuth'
 import { useSignInProvider } from '../hooks/useSignInProvider'
 import { Credential } from '@/domain'
-import { CredentialDisplay, SignInForm, UserActions, UserProfile } from './auth'
-import { CloseButton, Dialog, IconButton, VStack } from '@chakra-ui/react'
+import { CredentialDisplay, SignInForm, UserProfile } from './auth'
+import { Button, CloseButton, Dialog, IconButton, VStack } from '@chakra-ui/react'
 import { OpenChangeDetails } from '@zag-js/dialog'
 import { toaster } from '@presentation/ui/toaster-config'
 import { FaUserAstronaut } from 'react-icons/fa'
@@ -80,22 +80,22 @@ export function AuthModal() {
                             </VStack>
                         ) : (
                             <VStack align="stretch" gap={6}>
+                                <UserProfile currentUser={currentUser} />
                                 {credential && (
                                     <CredentialDisplay
                                         credential={credential}
                                         currentUser={currentUser}
+                                        onSignOut={() => setCredential(null)}
                                     />
                                 )}
-
-                                <UserProfile currentUser={currentUser} />
-
-                                <UserActions
-                                    onSignOut={() => setCredential(null)}
-                                    onDeleteAccount={() => setCredential(null)}
-                                />
                             </VStack>
                         )}
                     </Dialog.Body>
+                    <Dialog.Footer>
+                        <Dialog.ActionTrigger asChild>
+                            <Button variant="outline">Close</Button>
+                        </Dialog.ActionTrigger>
+                    </Dialog.Footer>
                 </Dialog.Content>
             </Dialog.Positioner>
         </Dialog.Root>
